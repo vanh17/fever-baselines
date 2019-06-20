bb9afa37e60f6297f0d1eb714cfa8794130fdf0b
Henry Van <vanh17@hanover.edu>
2019-06-20 07:32:50 -0700

    Added the spliting script for creating train, dev, test sets.

---
+++ src/scripts/fake-science/train_dev_test.py
@@ -0,0 +1,22 @@
+import sys
+import io
+
+
+def main():
+    if len(sys.argv) != 5:
+        exit("[usage] python3 train_dev_test.py path_to_splitting_file portion_of_training portion_of_dev path_to_output_folder")
+        portion_train = int(sys.argv[2])
+        portion_dev = int(sys.argv[3])
+    with io.open(sys.argv[1]) as input, io.open(sys.argv[4]+"/train.jsonl") as train, io.open(sys.argv[4]+"/dev.jsonl") as dev, io.open(sys.argv[4]+"/test.jsonl") as test:
+        lines = input.read().splitlines()
+        length = len(lines)
+        train_list =  lines[:int(portion_train*length/100)]
+        dev_list = lines[int(portion_train*length/100):int((portion_train + portion_dev)*length/100)]
+        test_list = lines[int((portion_train + portion_dev)*length/100):]
+        train.write("\n".join(train_list))
+        dev.write("\n".join(dev_list))
+        test.write("\n".join(test_list))
+
+
+if __name__ == '__main__':
+    main()
