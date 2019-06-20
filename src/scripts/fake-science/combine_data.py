4f9e0faa789807dc52388a41b6722ca7c27f3bed
Henry Van <vanh17@hanover.edu>
2019-06-20 07:57:36 -0700

    Modified the code so that we only need to pass the folder where the files are. This will combine 3 set at one run

--- src/scripts/fake-science/combine_data.py
+++ src/scripts/fake-science/combine_data.py
@@ -14,11 +14,16 @@ def combine(combined, file1, file2, multiply):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('combined', type=str, help='/path/to/combined/file.jsonl')
-    parser.add_argument('target', type=str, help='/path/to/target/file.jsonl')
-    parser.add_argument('source',type=str, help='/path/to/source/file.jsonl')
+    parser.add_argument('combined', type=str, help='/path/to/combined_folder/')
+    # pass the value to target. This is the folder where you want to store newly 3 dev, train, test set.
+    parser.add_argument('target', type=str, help='/path/to/target_folder/')
+    # pass the value to source. This is the folder where you want to store newly 3 dev, train, test set.
+    parser.add_argument('source',type=str, help='/path/to/source_folder/')
     parser.add_argument('multiply', type=int, help='multiplying factor')
     args = parser.parse_args()
-    combine(args.combined, args.target, args.source, args.multiply)
+    # code for creating train, dev, test respectively
+    combine(args.combined + "train.jsonl", args.target + "train_fever.jsonl", args.source + "train.jsonl", args.multiply)
+    combine(args.combined + "dev.jsonl", args.target + "dev_fever.jsonl", args.source + "dev.jsonl", args.multiply)
+    combine(args.combined + "test.jsonl", args.target + "test_fever.jsonl", args.source + "test.jsonl", args.multiply)
     # combine('train.ns.pages.p1.jsonl', 'fake-science-db.jsonl')
 
