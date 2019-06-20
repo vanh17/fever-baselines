6b0a38af27c0a10199a2e17042ac4c0edd63cc6b
Henry Van <vanh17@hanover.edu>
2019-06-20 06:57:05 -0700

    Update the query

--- src/scripts/fake-science/sqltojson.py
+++ src/scripts/fake-science/sqltojson.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         exit("[usage]: python3 sqltojson.py table_name [climate|astronomy|evolution|physics|...] path_to_output_folder")
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM " + sys.argv[1] + " WHERE category='"+ sys.argv[1] + "'")
+    cur.execute("SELECT * FROM " + sys.argv[1]) # + " WHERE category='"+ sys.argv[1] + "'")
     row_headers = [x[0] for x in cur.description]  # this will extract row headers
 
     result = cur.fetchall()
