4634c7c064a47cfd8ab5ffd8441d50303a3f296c
Henry Van <vanh17@hanover.edu>
2019-06-20 06:48:54 -0700

    Update the query, it now can access multiple table within the dataset

--- src/scripts/fake-science/sqltojson.py
+++ src/scripts/fake-science/sqltojson.py
@@ -11,15 +11,15 @@ mydb = mysql.connector.connect(
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        exit("[usage]: python3 sqltojson.py [climate|astronomy|evolution|physics|...] path_to_output_folder")
+        exit("[usage]: python3 sqltojson.py table_name [climate|astronomy|evolution|physics|...] path_to_output_folder")
     cur = mydb.cursor()
-    cur.execute("SELECT sourceID, student, url, category, reality, cesspool, keywords, title, body, taggedBy, manuallySkipped FROM sources WHERE category='"+ sys.argv[1] + "'")
+    cur.execute("SELECT * FROM " + sys.argv[1] + " WHERE category='"+ sys.argv[1] + "'")
     row_headers = [x[0] for x in cur.description]  # this will extract row headers
 
     result = cur.fetchall()
     json_data = []
 
-    f = open(sys.argv[2] + "/" + sys.argv[1] + "-corpus.jsonl","w+")
+    f = open(sys.argv[3] + "/" + sys.argv[2] + "-corpus.jsonl", "w+")
     for x in result:
         f.write(json.dumps(dict(zip(row_headers, x))) + '\n')
 
