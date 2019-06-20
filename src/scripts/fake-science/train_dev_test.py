f8d0764199de3b7a7d78e25329a7bc1a5e695586
Henry Van <vanh17@hanover.edu>
2019-06-20 07:38:16 -0700

    move portion_train/dev to under with...open command

--- src/scripts/fake-science/train_dev_test.py
+++ src/scripts/fake-science/train_dev_test.py
@@ -5,9 +5,9 @@ import io
 def main():
     if len(sys.argv) != 5:
         exit("[usage] python3 train_dev_test.py path_to_splitting_file portion_of_training portion_of_dev path_to_output_folder")
+    with io.open(sys.argv[1]) as input, io.open(sys.argv[4]+"/train.jsonl", "w+") as train, io.open(sys.argv[4]+"/dev.jsonl", "w+") as dev, io.open(sys.argv[4]+"/test.jsonl", "w+") as test:
         portion_train = int(sys.argv[2])
         portion_dev = int(sys.argv[3])
-    with io.open(sys.argv[1]) as input, io.open(sys.argv[4]+"/train.jsonl", "w+") as train, io.open(sys.argv[4]+"/dev.jsonl", "w+") as dev, io.open(sys.argv[4]+"/test.jsonl", "w+") as test:
         lines = input.read().splitlines()
         length = len(lines)
         train_list =  lines[:int(portion_train*length/100)]
