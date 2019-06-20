beb44c6535feb68555c658f5a3b0ca014ae920a6
Henry Van <vanh17@hanover.edu>
2019-06-20 07:35:54 -0700

    "Fixed the open command"

--- src/scripts/fake-science/train_dev_test.py
+++ src/scripts/fake-science/train_dev_test.py
@@ -7,7 +7,7 @@ def main():
         exit("[usage] python3 train_dev_test.py path_to_splitting_file portion_of_training portion_of_dev path_to_output_folder")
         portion_train = int(sys.argv[2])
         portion_dev = int(sys.argv[3])
-    with io.open(sys.argv[1]) as input, io.open(sys.argv[4]+"/train.jsonl") as train, io.open(sys.argv[4]+"/dev.jsonl") as dev, io.open(sys.argv[4]+"/test.jsonl") as test:
+    with io.open(sys.argv[1]) as input, io.open(sys.argv[4]+"/train.jsonl", "w+") as train, io.open(sys.argv[4]+"/dev.jsonl", "w+") as dev, io.open(sys.argv[4]+"/test.jsonl", "w+") as test:
         lines = input.read().splitlines()
         length = len(lines)
         train_list =  lines[:int(portion_train*length/100)]
